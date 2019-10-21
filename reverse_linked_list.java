ae9b2cf08519ec4a94a9e82dc0935f2a17a0b949
BI XI <542534206@qq.com>
2019-10-22 00:29:48 +1100

    Add files via upload

---
+++ reverse_linked_list.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class reverse_linked_list {
+    public ListNode reverseList(ListNode head){
+        if(head == null || head.next==null){
+            return head;
+        }
+        ListNode cur,pre,temp;
+        pre = null;
+        while(head != null){
+            temp = head.next;
+            head.next = pre;
+            pre = head;
+            head = temp;
+        }
+        return pre;
+    }
+}
